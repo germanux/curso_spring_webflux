54e32202fa160f5b1b6634a5ee9b1d61d6b55b04
German <germanux@gmail.com>
2019-03-20 20:03:42 +0100

    Implementando flujo temperatura pero con webflux y EmitterProcessor

--- 1_intro_prog_reactiva/src/main/java/com/intro/d_rest_temperatura/SensorTemperatura.java
+++ 1_intro_prog_reactiva/src/main/java/com/intro/d_rest_temperatura/SensorTemperatura.java
@@ -12,26 +12,32 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class SensorTemperatura {
-	private final ApplicationEventPublisher publicador;
+	public final ApplicationEventPublisher publicador;
 	private final Random rnd = new Random();
 	private final ScheduledExecutorService ejecutor = 
 		Executors.newSingleThreadScheduledExecutor();
 	
+	
 	public SensorTemperatura(ApplicationEventPublisher publicador) {
 		this.publicador = publicador;
+		System.out.println(">>> Constructor SensorTemperatura ");
 	}
 	@PostConstruct
 	public void startProcessing() {
+		System.out.println(">>> SensorTemperatura - startProcessing()");
 		this.ejecutor.schedule(this::probe, 1, TimeUnit.SECONDS);
+		System.out.println(">>> SensorTemperatura - startProcessing() FIN");
+		
 	}
 	// Calcular temperatura
 	public void probe() {
+		System.out.println(">>> SensorTemperatura - PROBE()");
 		double temperatura = 16 + rnd.nextGaussian() * 10;	// Número similar a grados
 		Temperatura temp = new Temperatura(temperatura);
 		
 		publicador.publishEvent(temp);
 		
 		ejecutor.schedule(this::probe, rnd.nextInt(5000), TimeUnit.MILLISECONDS);	
-		System.out.println("Temperatura enviada " + temp.getGrados());
+		System.out.println(">>> SensorTemperatura - PROBE() - Enviado: " + temp.getGrados());
 	}	
 }
